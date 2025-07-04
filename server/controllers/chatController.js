045e0ce037102b2fc8e9430e465cefc62ad09765
Tanya Mandloi05 <tanyamandloi2003@gmail.com>
2025-07-04 23:42:43 +0530

    built delete chat functionality

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 // create chat or member
 const createChat = async (req, res) => {
   try {
-    const currentUserId = req.auth.userId; //from your requireAuth
+    const currentUserId = req.auth.userId; //from  requireAuth
     const currentUser = await User.findOne({ clerkId: currentUserId });
     const { otherUserId } = req.body;
     if (!otherUserId)
@@ -42,4 +42,53 @@ const getChatsForUser = async(req, res) => {
   }
 }
 
-module.exports = {createChat, getChatsForUser};
+// will be used in future for group chats 
+// const removeMember = async(req, res) => {
+//   try {
+//     let currentUserId = req.auth.userId;
+//     const currentUser = await User.findOne({clerkId: currentUserId});
+//     let otherUserId = req.body;
+//      if (!otherUserId) {
+//       return res.status(400).json({ message: "Other user ID is required." });
+//     }
+//     let chat = Chat.findOne({members:{$all:[currentUser, otherUserId]}});
+//     if(!chat) {
+//       return res.status(404).json({message:"chat not found"});
+//     }
+
+//     chat.members.pull(otherUserId);
+//     await chat.save();
+//     if(chat.members.length < 2) {
+//       await chat.findByIdAndDelete(chat._id);
+//       return res.status(200).json({ message: "Member removed & chat deleted as only one member left." });
+//     }
+//     return res.status(200).json({message: "member deleted successfully", chat});
+//   } catch(err) {
+//     console.log(err);
+//     res.status(500).json({message: "internal server error"});
+//   }
+// };
+
+//deleting chat for curr user if it want to 
+const deleteChat = async(req, res) => {
+  try {
+    const chatId = await req.params.id;
+    const currentUser = req.auth.userId;
+    const currentUserDoc = await User.findOne({ clerkId: currentUser });
+    const currentUserId = currentUserDoc._id;
+    const chat = await Chat.findById(chatId);
+    if(!chat) {
+      return res.status(404).json({message: "chat not found"});
+    }
+    if(!chat.members.includes(currentUserId)) {
+      return res.status(403).json({ message: "You are not a member of this chat" });
+    }
+    await chat.deleteOne();
+    res.status(200).json({ message: "Chat deleted successfully!" });
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({message: "internal server error"});
+  }
+}
+
+module.exports = {createChat, getChatsForUser, deleteChat};
--- server/routes/chatRoute.js
+++ server/routes/chatRoute.js
@@ -1,9 +1,12 @@
-const {createChat, getChatsForUser} = require("../controllers/chatController");
+const {createChat, getChatsForUser, deleteChat} = require("../controllers/chatController");
 const { requireAuth } = require("../middlewares/authMiddleware");
 const express = require('express');
 const { router } = express();
 
 router.post("/chat", requireAuth, createChat);
 router.get("/Allchats", requireAuth, getChatsForUser);
+router.delete("/:id", requireAuth, deleteChat);
+// router.post("/removeMember", requireAuth, removeMember);
+
 
 module.exports = router;
